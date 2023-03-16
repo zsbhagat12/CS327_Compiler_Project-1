0030ebb58a356985b8bea959b14c2763f0d1e7f6
ArunMani1111 <76490044+ArunMani1111@users.noreply.github.com>
2023-03-17 03:44:22 +0530

    lexer fixes

--- lexer.py
+++ lexer.py
@@ -1,14 +1,13 @@
 from dataclasses import dataclass
 import sys
 
+# punctuators
 EOF           = 'EOF'
 ID            = 'ID'
 INTEGER_CONST = 'INTEGER_CONST'
 REAL_CONST    = 'REAL_CONST'
 LPAREN        = 'LPAREN'
 RPAREN        = 'RPAREN'
-
-#punctuators
 SEMI          = 'SEMI'
 DOT           = 'DOT'
 COLON         = 'COLON'
@@ -20,22 +19,21 @@ PLUS          = 'PLUS'
 MINUS         = 'MINUS'
 MUL           = 'MUL'
 FLOAT_DIV     = 'FLOAT_DIV'
-MODULO        = 'MODULO'
-POWER         = 'POWER'
 EQEQ          = 'EQEQ'
 NOTEQ         = 'NOTEQ'
 GT            = 'GT'
 LT            = 'LT'
 GTEQ          = 'GTEQ'
 LTEQ          = 'LTEQ'
-AND           = 'AND'
-OR            = 'OR'
+MODULO        = 'MODULO'
+AND           = "AND"
+OR            = "OR"
 PLUSEQ        = 'PLUSEQ'
 MINUSEQ       = 'MINUSEQ'
-MULEQ         = 'MULEQ'
-FLOAT_DIVEQ   = 'FLOAT_DIVEQ'
-MODULOEQ      = 'MODULOEQ'
 POWEREQ       = 'POWEREQ'
+MULEQ         = 'MULEQ'
+FLOAT_DIVEQ   = "FLOAT_DIVEQ"
+POWER         = 'POWER'
 
 # keywords
 PROGRAM       = 'PROGRAM'
@@ -52,16 +50,15 @@ THEN          = 'THEN'
 ELSE          = 'ELSE'
 WHILE         = 'WHILE'
 DO            = 'DO'
-STRING        = 'STRING'
 PRINT         = 'PRINT'
+STRING        = 'STRING'
 FUNCTION      = 'FUNCTION'
 RETURN        = 'RETURN'
 FOR           = 'FOR'
-RANGE         = 'RANGE'
-BY            = 'BY'
-TO            = 'TO'
-BREAK         = 'BREAK'
-
+RANGE         = "RANGE"
+BY            = "BY"
+TO            = "TO"
+BREAK         = "BREAK"
 # literals
 TRUE          = "TRUE"
 FALSE         = "FALSE"
@@ -71,7 +68,7 @@ FALSE         = "FALSE"
 class Token:
     type: str
     value: object
-    # an example for the representation of a class instance: Token(PLUS,'+') 
+    
     def __str__(self):
         return 'Token({type}, {value})'.format(
             type=self.type,
@@ -89,26 +86,29 @@ KEYWORDS = {
     'DIV': Token('INTEGER_DIV', 'DIV'),
     'BEGIN': Token('BEGIN', 'BEGIN'),
     'END': Token('END', 'END'),
+    'FOR': Token('FOR', 'FOR'),
     'TRUE': Token('TRUE', True),
     'FALSE': Token('FALSE', False),
-    'STRING': Token('STRING', 'STRING'),
-    'FOR': Token('FOR', 'FOR'),
     'IF': Token('IF', 'IF'),
     'THEN': Token('THEN', 'THEN'),
     'ELSE': Token('ELSE', 'ELSE'),
     'PRINT': Token('PRINT', 'PRINT'),
     'WHILE': Token('WHILE', 'WHILE'),
     'DO': Token('DO', 'DO'),
+    'STRING': Token('STRING', 'STRING'),
     'FUNCTION': Token('FUNCTION', 'FUNCTION'),
     'RETURN': Token('RETURN', 'RETURN'),
     'BY': Token('BY', 'BY'),
     'TO': Token("TO", "TO"),
-    'BREAK': Token("BREAK", "BREAK")
+    'BREAK': Token("BREAK", "BREAK"),
+    # 'PRINT': Token('PRINT', ';'),
+    
 }
 
 
 class Lexer(object):
     def __init__(self, text):
+        
         self.text = text                        # stream of input
         self.pos = 0                            # current position in the stream
         self.lineNum = 1                        # line number in code
@@ -120,6 +120,7 @@ class Lexer(object):
         else:
             self.curChar = self.text[self.pos]      # current character in the stream
         self.curLine = self.curChar             # current line of program read till now
+        
 
     def error(self):
         sys.exit('Invalid character')
@@ -134,7 +135,13 @@ class Lexer(object):
             self.curChar = self.text[self.pos]
             self.curLine += self.curChar
 
-    def skipSpaces(self):                        # to skip white spacses
+    def peek(self):                             # returns the lookahead character
+        if self.pos + 1 > len(self.text) - 1:
+            return None
+        else:
+            return self.text[self.pos + 1]
+
+    def skipSpaces(self):                       # to skip white spacses
         while self.curChar is not None and self.curChar.isspace():
             if self.curChar == '\n':
                 self.lineNum+=1
@@ -143,9 +150,9 @@ class Lexer(object):
             self.nextChar()
 
     def skipComments(self):
-        #while self.curChar != '}':
-        #    self.nextChar()
-        #self.nextChar()                         # to skip the closing curly brace as well
+        # while self.curChar != '}':
+        #     self.nextChar()
+        # self.nextChar()                         # to skip the closing curly brace as well
         noOfstartBraces = 1
         while noOfstartBraces != 0 :
             self.nextChar()
@@ -153,14 +160,7 @@ class Lexer(object):
                 noOfstartBraces+=1
             elif self.curChar == '}':
                 noOfstartBraces-=1
-        self.nextChar()         
-        
-
-    def peek(self):                              # returns the lookahead character
-        if self.pos + 1 > len(self.text) - 1:
-            return None
-        else:
-            return self.text[self.pos + 1]
+        self.nextChar()                         # to skip the closing curly brace as well
 
     def number(self):                           
         # Consume all the consecutive digits and decimal if present.
@@ -230,7 +230,7 @@ class Lexer(object):
                 if self.peek() == '=':
                     self.nextChar()
                     self.nextChar()
-                    return Token(ASSIGN, ':=')
+                    return Token(ASSIGN, '=') # :=
                 else:
                     self.nextChar()
                     return Token(COLON, ':')
@@ -241,12 +241,13 @@ class Lexer(object):
                     self.nextChar()
                     return Token(OR, "||")
             
+
             if self.curChar == "&":
                 if self.peek() == "&":
                     self.nextChar()
                     self.nextChar()
                     return Token(AND, "&&")
-                
+
             if self.curChar == '>':
                 if self.peek() == '=':
                     self.nextChar()
@@ -268,7 +269,7 @@ class Lexer(object):
                 else:
                     self.nextChar()
                     return Token(LT, '<')
-
+            
             if self.curChar == '@':
                 if self.peek() == '=':
                     self.nextChar()
@@ -277,19 +278,14 @@ class Lexer(object):
                 else:
                     self.nextChar()
                     return Token(POWER, '**')
-                    
-            if self.curChar == '%':
-                if self.peek() == '=':
-                    self.nextChar()
-                    self.nextChar()
-                    return Token(MODULOEQ, '%=')
-                else:
-                    self.nextChar()
-                    return Token(MODULO, '%')                    
-                    
+                
+            if self.curChar == "%":
+                self.nextChar()
+                return Token(MODULO, '%')
+
             if self.curChar == '=':
                 self.nextChar()
-                return Token(EQEQ, '=')
+                return Token(EQEQ, '==') # =
 
             if self.curChar == '+':
                 if self.peek() == '=':
@@ -346,10 +342,10 @@ class Lexer(object):
             if self.curChar == ',':
                 self.nextChar()
                 return Token(COMMA, ',')
-  
+            
             if self.curChar == '"':
                 self.nextChar()
-                return self.Stringlex()  
+                return self.Stringlex()          
 
             self.error()
 
